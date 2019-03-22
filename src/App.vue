9753f65a50232b4a22fec09c2bc93d98f66872f7
Nithipong <nithipong.jamd@gmail.com>
2019-03-23 06:23:19 +0700

    install vuex, bulma

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,33 @@
 <template>
   <div id="app">
-    <h1>It work</h1>
+    <main class="container">
+      <div class="columns">
+        <div class="column">
+          <h1 class="title">
+            Little Brown Book Shop
+          </h1>
+          listing
+        </div>
+        <div class="column is-narrow">
+          <div class="cart-container">
+            Cart
+          </div>
+        </div>
+      </div>
+    </main>
   </div>
 </template>
 
 <script>
-
 export default {
   name: 'app'
 }
 </script>
 
-<style>
+<style lang="sass" scope>
+.cart-container
+  min-width: 300px
+  min-height: 100vh
+  background-color: #f5f5f5
+
 </style>
